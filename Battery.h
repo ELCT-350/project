f524fa4acd74fe4eebc195ba3f31e588ebfabf89
Davis Hobbs <dkh2085@gmail.com>
2022-04-22 09:33:31 -0400

    Added Functions to build the VIN and R/C values at each SOC step. Functions are named Get___(soc).

--- Battery.h
+++ Battery.h
@@ -64,18 +64,54 @@ void Battery::Step(double t, double dt)
     AddBEquivalent(nodej, -b);
 }
 
-double Capacitor::GetVoltage()
+
+double Battery::GetTerminalVoltage()
 {
     return GetStateDifference(nodei, nodej);
 }
 
-double Capacitor::GetCurrent()
+double Battery::GetTerminalCurrent()
 {
     // current = g * v - b:
     return C / GetTimeStep() * GetVoltage() - GetBEquivalent(nodei);
 }
 
-double Capacitor::GetPower()
+double Battery::GetSOC(double socOld, double wH, double h)
 {
-    return GetVoltage() * GetCurrent();
+    return socOld + (GetVoltage() * GetCurrent()*h)/(wH*3600);
+}
+
+double Battery::GetValue(double soc, double A, double k, double a0, double a1, double a2, double a3)
+{
+    return A * exp(k * soc) + a0 + a1 * soc + a2 * (soc) ^ 2 + a3 * (soc) ^ 3;
+}
+
+double Battery::GetVIN(double soc)
+{
+    return GetValue(soc, -1.031, -35.0, 3.685, 0.2156, -0.1178, 0.3201);
+}
+
+double Battery::GetRIN(double soc)
+{
+    return GetValue(soc, 0.1562, -24.37, 0.07446, 0, 0, 0);
+}
+
+double Battery::GetRT1(double soc)
+{
+    return GetValue(soc, 0.3208, -29.14, 0.04669, 0, 0, 0);
+}
+
+double Battery::GetRT2(double soc)
+{
+    return GetValue(soc, 6.603, -155.2, 0.04984, 0, 0, 0);
+}
+
+double Battery::GetCT1(double soc)
+{
+    return GetValue(soc, -752.9, -13.51, 703.6, 0, 0, 0);
+}
+
+double Battery::GetCT2(double soc)
+{
+    return GetValue(soc, -6056.0, -27.12, 4475.0, 0, 0, 0);
 }
